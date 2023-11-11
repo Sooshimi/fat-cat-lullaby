ff6c069bfb31f7111ff1e7ec008d61279e6860b6
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-11-11 21:08:09 +0000

    Add camera drag, and jump WIP

--- scenes/fat_cat.gd
+++ scenes/fat_cat.gd
@@ -4,7 +4,13 @@ var speed : float = 100
 var target : Vector2
 var is_moving : bool
 var is_jumping : bool
+var is_grounded : bool = true
 var collision
+var i : int
+
+@onready var roll_cooldown : Node = $RollCooldown
+@onready var jump_duration : Node = $JumpDuration
+@onready var jump_window : Node = $JumpWindow
 
 func _ready() -> void:
 	# Stop cat moving at first launch of game
@@ -21,14 +27,30 @@ func get_input(delta) -> void:
 	if Input.is_action_just_pressed("left_click") && !is_moving:
 		target = get_global_mouse_position()
 		is_moving = true
+		i += 1
 	else:
-		if global_position.distance_to(target) < 10:
+		# 'i' variable is to stop this block running more than once. 
+		if global_position.distance_to(target) < 3 && i == 1:
+			i = 0
 			is_moving = false
+			# Start window where jump action is allowed shortly during slide
+			jump_window.start()
 	
 	collision = move_and_collide(velocity * delta, false, 0.00)
 	
 	if is_moving:
+		# Move cat
 		velocity = global_position.direction_to(target) * speed
 	else:
-		# Short cat slide after rolling
+		# Short cat slide after rolling until stopped
 		velocity = velocity.move_toward(Vector2.ZERO, 6)
+		
+		# Allow one jump during this window
+		if Input.is_action_just_pressed("jump") && !jump_window.is_stopped() && is_grounded:
+			is_grounded = false
+			jump_duration.start()
+			print("Jump")
+
+func _on_jump_duration_timeout():
+	is_grounded = true
+	print("Landed")
