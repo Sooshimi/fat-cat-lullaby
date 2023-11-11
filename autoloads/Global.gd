99b74e5039d83061e2bb27a296284f4e23b2bc8b
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-11-11 13:49:13 +0000

    Add lullaby check mechanic

--- autoloads/Global.gd
+++ autoloads/Global.gd
@@ -6,3 +6,13 @@ var lullaby : Array = ["e", "d", "c"]
 func _unhandled_input(_event):
 	if Input.is_action_pressed("escape"):
 		get_tree().quit()
+
+func check_key_order():
+	for i in keys_played.size():
+		var letter = keys_played[i]
+		if letter != lullaby[i]:
+			keys_played = []
+	
+	if keys_played == lullaby:
+		print("Correct order!")
+		keys_played = []
--- scenes/piano.gd
+++ scenes/piano.gd
@@ -4,6 +4,8 @@ extends Node2D
 @onready var key_d := $KeyDArea/AudioStreamPlayer
 @onready var key_e := $KeyEArea/AudioStreamPlayer
 
+signal key_played
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,11 +14,20 @@ func _ready():
 func _process(delta):
 	pass
 
+func _emit_key_played():
+	key_played.connect(Global.check_key_order)
+	key_played.emit()
+
+func play_sound_and_append_and_emit(key_sound_node:Node, key_letter:String):
+	key_sound_node.play()
+	Global.keys_played.append(key_letter)
+	_emit_key_played()
+
 func _on_key_c_area_body_entered(body):
-	key_c.play()
+	play_sound_and_append_and_emit(key_c, "c")
 
 func _on_key_d_area_body_entered(body):
-	key_d.play()
+	play_sound_and_append_and_emit(key_d, "d")
 
 func _on_key_e_area_body_entered(body):
-	key_e.play()
+	play_sound_and_append_and_emit(key_e, "e")
