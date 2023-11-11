861b35ca8fa79936a2e4da33e6d131a6b2f6bc04
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-11-11 15:11:04 +0000

    Add cat slide

--- scenes/baby.gd
+++ scenes/baby.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var speed = 65
+var speed = 50
 var player : Node
 var relative_direction : Vector2
 var collision
--- scenes/fat_cat.gd
+++ scenes/fat_cat.gd
@@ -2,15 +2,30 @@ extends CharacterBody2D
 
 var speed := 100
 var target : Vector2
+var is_moving : bool
+var collision
 
 func _physics_process(delta):
-	get_input()
+	get_input(delta)
 	
-	if global_position.distance_to(target) > 1:
-		move_and_slide()
+	# Stop player from clicking whilst cat is moving
+	if global_position.distance_to(target) > 10:
+		is_moving = true
+	else:
+		is_moving = false
+	
+#	if collision:
+#		# Allows player to slide on walls
+#		velocity = velocity.slide(collision.get_normal())
 
-func get_input():
-	if Input.is_mouse_button_pressed(1):
+func get_input(delta):
+	if Input.is_action_just_pressed("left_click") && !is_moving:
 		target = get_global_mouse_position()
 	
-	velocity = global_position.direction_to(target) * speed
+	collision = move_and_collide(velocity * delta, false, 0.00)
+	
+	if is_moving:
+		velocity = global_position.direction_to(target) * speed
+	else:
+		# Short cat slide after rolling
+		velocity = velocity.move_toward(Vector2.ZERO, 6)
