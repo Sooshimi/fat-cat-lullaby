06d1b49ec94eeda3afb8d52591108acc8e61f266
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-11-13 19:51:07 +0000

    Add reaction time ranges

--- scenes/baby.gd
+++ scenes/baby.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
 var speed : int = 40
+const SLEEPY_REACTION_TIME : Array = [1.0, 1.2]
+const NORMAL_REACTION_TIME : Array = [0.4, 0.6]
+const ANGRY_REACTION_TIME : Array = [0.1, 0.3]
 
 @export var animation_tree : Node
 @export var player : Node2D
 @onready var nav_agent : Node = $NavigationAgent2D
+@onready var new_path_timer : Node = $NewPathTimer
 
 func _ready() -> void:
 	animation_tree.active = true
@@ -30,5 +34,9 @@ func play_move_animations() -> void:
 		animation_tree.set("parameters/Idle/blend_position", velocity)
 		animation_tree.set("parameters/Run/blend_position", velocity)
 
+func set_reaction_time(reaction_time:Array) -> void:
+	new_path_timer.wait_time = randf_range(reaction_time[0], reaction_time[1])
+
 func _on_timer_timeout() -> void:
 	make_path()
+	set_reaction_time(NORMAL_REACTION_TIME)
