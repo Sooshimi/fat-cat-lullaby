e1b5416ec25ad61d840fed1974e3f11a8d70cfe5
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-11-14 22:36:53 +0000

    Add quit button to pause menu

--- scenes/ui/pause_menu.gd
+++ scenes/ui/pause_menu.gd
@@ -1,9 +1,12 @@
 extends Control
 
-func _ready():
+func _ready() -> void:
 	hide()
 
-func _on_continue_pressed():
+func _on_continue_pressed() -> void:
 	get_tree().paused = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	hide()
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
