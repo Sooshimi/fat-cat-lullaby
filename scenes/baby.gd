3f51e449f62964d96ee9886d3e6cd3cb72fc4cac
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-11-14 11:07:47 +0000

    Add emote trigger mechanic

--- scenes/baby.gd
+++ scenes/baby.gd
@@ -1,17 +1,29 @@
 extends CharacterBody2D
 
-var speed : int = 40
+const SPEED_SLEEPY : int = 20
+const SPEED_NORMAL : int = 40
+const SPEED_ANGRY : int = 60
+var speed : int = SPEED_NORMAL
+
+var emote_sleepy = load("res://assets/emotes/emote_sleeps.png")
+var emote_angry = load("res://assets/emotes/emote_anger.png")
+
 const SLEEPY_REACTION_TIME : Array = [1.0, 1.2]
 const NORMAL_REACTION_TIME : Array = [0.4, 0.6]
 const ANGRY_REACTION_TIME : Array = [0.1, 0.3]
+var current_reaction_time : Array = NORMAL_REACTION_TIME
 
+var collision
 @export var animation_tree : Node
 @export var player : Node2D
+@onready var emote : Node2D = $Emote
+@onready var emote_timer : Node = $EmoteTimer
 @onready var nav_agent : Node = $NavigationAgent2D
 @onready var new_path_timer : Node = $NewPathTimer
 
 func _ready() -> void:
 	animation_tree.active = true
+	emote.hide()
 
 func _physics_process(_delta) -> void:
 	var dir = to_local(nav_agent.get_next_path_position()).normalized()
@@ -34,9 +46,29 @@ func play_move_animations() -> void:
 		animation_tree.set("parameters/Idle/blend_position", velocity)
 		animation_tree.set("parameters/Run/blend_position", velocity)
 
-func set_reaction_time(reaction_time:Array) -> void:
-	new_path_timer.wait_time = randf_range(reaction_time[0], reaction_time[1])
+func set_reaction_time(current_reaction_time) -> void:
+	new_path_timer.wait_time = randf_range(current_reaction_time[0], current_reaction_time[1])
 
 func _on_timer_timeout() -> void:
 	make_path()
-	set_reaction_time(NORMAL_REACTION_TIME)
+	set_reaction_time(current_reaction_time)
+
+func trigger_emote(expression:String) -> void:
+	if emote_timer.is_stopped():
+		if expression == "sleepy":
+			current_reaction_time = SLEEPY_REACTION_TIME
+			speed = SPEED_SLEEPY
+			set_emote(emote_sleepy)
+		elif expression == "angry":
+			current_reaction_time = ANGRY_REACTION_TIME
+			speed = SPEED_ANGRY
+			set_emote(emote_angry)
+
+func set_emote(emote_icon) -> void:
+	emote.texture = emote_icon
+	emote.show()
+	emote_timer.start()
+
+func _on_emote_timer_timeout():
+	speed = SPEED_NORMAL
+	emote.hide()
--- scenes/items/toy_police_car.gd
+++ scenes/items/toy_police_car.gd
@@ -2,6 +2,8 @@ extends RigidBody2D
 
 @onready var note : Node = $AudioStreamPlayer
 @onready var note_timer : Node = $NoteTimer
+@onready var baby = get_parent().get_node("Baby")
+@export var baby_emotion_trigger : String
 
 var key_c := load("res://audio/3C.ogg")
 var key_d := load("res://audio/3D.ogg")
@@ -9,7 +11,9 @@ var key_e := load("res://audio/3E.ogg")
 
 var tune : Array = [key_e, key_d, key_c]
 
-func _on_area_2d_body_entered(_body):
+func _on_area_2d_body_entered(_body) -> void:
+	baby.trigger_emote(baby_emotion_trigger)
+	
 	for key in tune:
 		note.stream = key
 		note.play()
