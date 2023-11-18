13a2e037a182e488262e482a7d491c9b963e26a3
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-11-18 13:37:41 +0000

    Add music to toys

--- scenes/items/toy_police_car.gd
+++ scenes/items/toy_police_car.gd
@@ -1,8 +1,9 @@
 extends RigidBody2D
 
-@onready var note : Node = $AudioStreamPlayer
 @onready var note_timer : Node = $NoteTimer
 @onready var baby : CharacterBody2D = get_parent().get_node("Baby")
+@onready var audio_player : AudioStreamPlayer = $AudioStreamPlayer
+@export var music : Resource
 @export var piano : Node
 
 @export_enum("c0", "c1", "cs1", "d1", "eb1", "e1", "f1", "fs1", "g1", "ab1",\
@@ -21,6 +22,7 @@ var tune_strings : Array
 
 func _ready() -> void:
 	load_keys()
+	audio_player.stream = music
 
 func load_keys() -> void:
 	var key_array : Array = [key_1, key_2, key_3]
@@ -35,11 +37,9 @@ func load_keys() -> void:
 
 func _on_area_2d_body_entered(_body) -> void:
 	baby.trigger_emote(baby_emotion_trigger)
+	audio_player.play()
 	
 	for i in tune_load.size():
-		var key_load = tune_load[i]
-		note.stream = key_load
-		note.play()
 		note_timer.start()
 		piano.trigger_piano_key(tune_strings[i], false)
 		await note_timer.timeout
