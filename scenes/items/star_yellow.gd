a1f3dcdff9f89e888efba1864e843729bb9f3a76
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-11-18 11:59:09 +0000

    Add star pick up sound

--- scenes/items/star_yellow.gd
+++ scenes/items/star_yellow.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var player : CharacterBody2D
+@onready var pickup_sound : AudioStreamPlayer = $AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +12,9 @@ func _process(delta):
 	pass
 
 func _on_body_entered(body):
+	pickup_sound.play()
+	hide()
 	get_parent().player_light.texture_scale += 0.3
 	get_parent().shadow_light.texture_scale += 0.3
+	await pickup_sound.finished
 	queue_free()
