f5be6a50de7d1a5eef30a67ee647fa0621489664
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-11-18 22:01:20 +0000

    Replace music with fade outs

--- autoloads/Audio.gd
+++ autoloads/Audio.gd
@@ -1,23 +1,7 @@
 extends Node
 
-var fade_out : bool = false
-
 @onready var toy_music : AudioStreamPlayer = $ToyMusic
-@onready var timer : Timer = $Timer
-
-func _process(delta) -> void:
-	if fade_out:
-		toy_music.volume_db = lerp(toy_music.volume_db, -80.0, 0.8 * delta)
-	
-		if toy_music.volume_db < -50.0:
-			toy_music.volume_db = 0.0
-			toy_music.stop()
-			fade_out = false
 
 func play_toy_music(music:Resource) -> void:
 	toy_music.stream = music
 	toy_music.play()
-	
-	timer.start()
-	await timer.timeout
-	fade_out = true
