73c14a3d851a33fe56df7aea1af137a79c8845fb
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2023-11-19 12:47:51 +0000

    Add in key pages

--- scenes/baby.gd
+++ scenes/baby.gd
@@ -12,6 +12,7 @@ const SLEEPY_REACTION_TIME : Array = [1.0, 1.2]
 const NORMAL_REACTION_TIME : Array = [0.4, 0.6]
 const ANGRY_REACTION_TIME : Array = [0.1, 0.3]
 var current_reaction_time : Array = NORMAL_REACTION_TIME
+var current_expression : String
 
 var collision
 @export var animation_tree : Node
@@ -59,6 +60,14 @@ func play_move_animations() -> void:
 	# If enemy not moving, travel to idle animation
 	if velocity == Vector2.ZERO:
 		animation_tree.get("parameters/playback").travel("Idle")
+	elif current_expression == "sleepy":
+		animation_tree.get("parameters/playback").travel("Run_Sleepy")
+		animation_tree.set("parameters/Idle/blend_position", velocity)
+		animation_tree.set("parameters/Run_Sleepy/blend_position", velocity)
+	elif current_expression == "angry":
+		animation_tree.get("parameters/playback").travel("Run_Angry")
+		animation_tree.set("parameters/Idle/blend_position", velocity)
+		animation_tree.set("parameters/Run_Angry/blend_position", velocity)
 	else:
 		# If enemy moving, travel to walk animation
 		# Set blend positions (directions) of animations based on relative direction
@@ -66,11 +75,11 @@ func play_move_animations() -> void:
 		animation_tree.set("parameters/Idle/blend_position", velocity)
 		animation_tree.set("parameters/Run/blend_position", velocity)
 		
-		if footsteps_timer.is_stopped():
-			footsteps_sound.pitch_scale = randf_range(0.9, 1.1)
-			footsteps_sound.volume_db = randf_range(footsteps_vol-2.0, footsteps_vol+2.0)
-			footsteps_sound.play()
-			footsteps_timer.start()
+	if velocity != Vector2.ZERO && footsteps_timer.is_stopped():
+		footsteps_sound.pitch_scale = randf_range(0.9, 1.1)
+		footsteps_sound.volume_db = randf_range(footsteps_vol-2.0, footsteps_vol+2.0)
+		footsteps_sound.play()
+		footsteps_timer.start()
 
 # Randomise reaction time within a range based on what emotional response is 
 # triggered (sleepy, normal, angry).
@@ -85,6 +94,7 @@ func _on_timer_timeout() -> void:
 func trigger_emote(expression:String) -> void:
 	# Trigger emote after emote cooldown
 	if emote_timer.is_stopped():
+		current_expression = expression
 		# Set baby speed and path finding reaction times based on response type
 		# from the collided toy
 		if expression == "sleepy":
